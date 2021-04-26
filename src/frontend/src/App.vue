bd660fc494d346b68f8acb278fc26d77e8dbf4d5
stvoid <stvoid.it@yandex.ru>
2021-04-26 16:01:32 +0300

    sticky drugged

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,41 +1,65 @@
 <template>
     <div class="container-fluid">
-        <h4>{{ prevDir }}</h4>
-        <div class="btn-group-vertical">
-            <button
-                class="btn btn-info btn-sm block mb-1"
-                @click="actionHandler({path: prevDir, name:'..', isDir: true})">
-                ...
-            </button>
-            <button
-                v-for="v in values"
-                :key="v.name"
-                :class="{'btn-primary': v.isDir, 'btn-outline-dark': !v.isDir}"
-                class="btn btn-sm block mb-1"
-                @click="actionHandler(v)">
-                {{ v.name }}
-                <template v-if="v.hrSize">
-                    [{{ v.hrSize }}]
+        <div class="row">
+            <div class="col">
+                <h4>{{ prevDir }}</h4>
+                <div class="btn-group-vertical">
+                    <button
+                        class="btn btn-info btn-sm block mb-1"
+                        @click="actionHandler({path: prevDir, name:'..', isDir: true})">
+                        ...
+                    </button>
+                    <button
+                        v-for="(v, index) in values"
+                        :id="index"
+                        :key="v.name"
+                        draggable="true"
+                        :class="{'btn-primary': v.isDir, 'btn-outline-dark': !v.isDir}"
+                        class="btn btn-sm block mb-1"
+                        @dragstart="dragStart"
+                        @click="actionHandler(v)">
+                        {{ v.name }}
+                        <template v-if="v.hrSize">
+                            [{{ v.hrSize }}]
+                            <div
+                                v-if="v.downloadProgress"
+                                class="progress">
+                                <div
+                                    :style="{width: `${v.downloadProgress}%`}"
+                                    class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                                    role="progressbar"
+                                    :aria-valuenow="v.downloadProgress"
+                                    aria-valuemin="0"
+                                    aria-valuemax="100" />
+                            </div>
+                        </template>
+                    </button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="sticky-col">
+                    <h4>список скачивания</h4>
                     <div
-                        v-if="v.downloadProgress"
-                        class="progress">
+                        class="droptarget"
+                        @dragover="(event) => {
+                            event.preventDefault();
+                        }"
+                        @drop="drop">
                         <div
-                            :style="{width: `${v.downloadProgress}%`}"
-                            class="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                            role="progressbar"
-                            :aria-valuenow="v.downloadProgress"
-                            aria-valuemin="0"
-                            aria-valuemax="100" />
+                            v-for="d in listDownload"
+                            :key="d.name">
+                            {{ `${d.path}/${d.name }` }}
+                        </div>
                     </div>
-                </template>
-            </button>
+                    <p>total: {{ totalDownloadSize }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-
-import { ref } from 'vue';
+import { ref,computed } from 'vue';
 import axios from "axios";
 export default {
     setup() {
@@ -43,6 +67,7 @@ export default {
         const prevDir = ref("");
         const api = axios.create();
         let values = ref([]);
+        let listDownload = ref([]);
 
         api.get("/fs").then(res => {
             prevDir.value = res.data.prevPath;
@@ -89,11 +114,31 @@ export default {
                 downloadFile(obj);
             }
         };
+
+        const dragStart = (event) => {
+            event.dataTransfer.setData("FileIndex", event.target.id);
+        };
+        const drop = (event) => {
+            event.preventDefault();
+            let dataIndex = event.dataTransfer.getData("FileIndex");
+            listDownload.value.push(values.value[dataIndex]);
+        };
+        const totalDownloadSize = computed(() => {
+            let totalSize = 0;
+            listDownload.value.forEach(e => {
+                totalSize+=e.size;
+            });
+            return totalSize;
+        });
         return {
             title,
             prevDir,
             values,
-            actionHandler
+            actionHandler,
+            listDownload,
+            dragStart,
+            drop,
+            totalDownloadSize
         };
     }
 };
@@ -103,5 +148,17 @@ export default {
 .block {
     border-radius: 0 !important;
     text-align: left !important;
+}
+.droptarget {
+
+    float: left;
+    width: 100%;
+    min-height: 200px;
+    padding: 10px;
+    border: 1px solid #aaaaaa;
+}
+.sticky-col {
+    position: sticky;
+    top: 2%;
 }
 </style>
