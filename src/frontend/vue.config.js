daffa101afef9aed9649d6e515443dfed1200091
stvoid <stvoid.it@yandex.ru>
2021-04-28 11:13:51 +0300

    conf

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     productionSourceMap: false,
+    runtimeCompiler: true,
     devServer: {
         proxy: 'http://localhost:9000'
     },
     pages: {
         index: "src/main.js"
-    }
+    },
+    outputDir: "../../build/static"
 };
